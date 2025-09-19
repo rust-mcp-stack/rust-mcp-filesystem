7540e62c67f0b3bb650b30d2b95d77f31a47dbbf
Ali Hashemi <programmer.ice@gmail.com>
2025-09-19 17:36:00 -0300

    add read_media tool

--- src/tools/read_multiple_text_files.rs
+++ src/tools/read_multiple_text_files.rs
@@ -1,16 +1,14 @@
-use std::path::Path;
-
+use crate::fs_service::FileSystemService;
 use futures::future::join_all;
 use rust_mcp_sdk::macros::{JsonSchema, mcp_tool};
 use rust_mcp_sdk::schema::TextContent;
 use rust_mcp_sdk::schema::{CallToolResult, schema_utils::CallToolError};
-
-use crate::fs_service::FileSystemService;
+use std::path::Path;
 
 #[mcp_tool(
-    name = "read_multiple_files",
-    title="Read Multiple Files",
-    description = concat!("Read the contents of multiple files simultaneously. ",
+    name = "read_multiple_text_files",
+    title="Read Multiple Text Files",
+    description = concat!("Read the contents of multiple text files simultaneously as text. ",
     "This is more efficient than reading files one by one when you need to analyze ",
     "or compare multiple files. Each file's content is returned with its ",
     "path as a reference. Failed reads for individual files won't stop ",
@@ -21,12 +19,12 @@ use crate::fs_service::FileSystemService;
     read_only_hint = true
 )]
 #[derive(::serde::Deserialize, ::serde::Serialize, Clone, Debug, JsonSchema)]
-pub struct ReadMultipleFilesTool {
+pub struct ReadMultipleTextFilesTool {
     /// The list of file paths to read.
     pub paths: Vec<String>,
 }
 
-impl ReadMultipleFilesTool {
+impl ReadMultipleTextFilesTool {
     pub async fn run_tool(
         params: Self,
         context: &FileSystemService,
@@ -37,7 +35,7 @@ impl ReadMultipleFilesTool {
             .map(|path| async move {
                 {
                     let content = context
-                        .read_file(Path::new(&path))
+                        .read_text_file(Path::new(&path))
                         .await
                         .map_err(CallToolError::new);
 
